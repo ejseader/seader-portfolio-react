48313d233aaffc3e88200680cb0cf205ffbf561c
Eric Seader <45432901+ejseader@users.noreply.github.com>
2023-05-18 00:01:00 -0400

    fixed spacing for skills and contact

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -3,16 +3,16 @@ import React from "react";
 const Contact = () => {
   return (
     <section className="bg-secondary px-5 text-white py-32" id="contact">
-      <div className="section-wrap">
+      <div className="container mx-auto flex flex-col justify-center align-middle max-w-3xl">
         <div className="section">
           <h2 className="text-4xl font-bold mb-6 divide-y border-b-[5px] w-[260px] border-[#00FFCA] pb-2">
             Contact
           </h2>
-          <form action="https://getform.io/f/6e48774f-f8ae-40a7-8144-a851b945e516" method="POST" className="flex flex-col mx-auto justify-center align-middle">
+          <form action="https://getform.io/f/6e48774f-f8ae-40a7-8144-a851b945e516" method="POST" className="flex flex-col mx-auto justify-center align-middle max-w-3xl">
             <input className="p-2 rounded" type="text" placeholder="Name" name="name" />
             <input className="my-4 p-2 rounded" type="email" placeholder="Email" name="email" />
             <textarea className="p-2 rounded" name="message" rows="10" placeholder="Message"></textarea>
-            <button className="text-black bg-accent rounded hover:bg-[#0A4D68] hover:text-white px-4 py-3">Let's Collab!</button>
+            <button className="text-black bg-accent rounded hover:bg-[#088395] hover:text-white px-4 py-3">Let's Collab!</button>
           </form>
         </div>
       </div>
--- src/components/Skills.js
+++ src/components/Skills.js
@@ -12,7 +12,7 @@ import Mongo from '../assets/mongo.png';
 const Skills = () => {
   return (
     <section className="bg-primary px-5 text-white py-32" id="skills">
-      <div className='max-w-[1000px] mx-auto p-4 flex flex-col justify-center w-full h-full'>
+      <div className='max-w-7xl mx-auto p-4 flex flex-col justify-center w-full h-full'>
           <div>
               <p className='text-4xl font-bold mb-6 divide-y border-b-[5px] w-[260px] border-[#00FFCA] pb-2'>Skills</p>
           </div>
